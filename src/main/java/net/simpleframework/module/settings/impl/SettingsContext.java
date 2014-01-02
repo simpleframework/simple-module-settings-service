b56ab1168387165157f990bb621b22582399714a
simpleframework <cknet@126.com>
2014-01-02 19:37:23 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/settings/impl/SettingsContext.java
+++ src/main/java/net/simpleframework/module/settings/impl/SettingsContext.java
@@ -3,6 +3,7 @@ package net.simpleframework.module.settings.impl;
 import static net.simpleframework.common.I18n.$m;
 import net.simpleframework.ctx.AbstractModuleContext;
 import net.simpleframework.ctx.Module;
+import net.simpleframework.ctx.permission.IPermissionConst;
 import net.simpleframework.module.settings.ISettingsContext;
 
 /**
@@ -12,6 +13,11 @@ import net.simpleframework.module.settings.ISettingsContext;
  *         http://www.simpleframework.net
  */
 public class SettingsContext extends AbstractModuleContext implements ISettingsContext {
+
+	@Override
+	public String getManagerRole() {
+		return IPermissionConst.ROLE_MANAGER;
+	}
 
 	@Override
 	protected Module createModule() {
